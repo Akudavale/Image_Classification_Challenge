5b7e190147edec10cce614965ec95db833ee46b8
Akudavale <kudavale3105@gmail.com>
2024-01-25 21:05:14 +0100

    update4

--- model.py
+++ model.py
@@ -1,12 +1,22 @@
 import torch
 import torch.nn as nn
 import torchvision
+from data import ChristmasImages
+
+Data = ChristmasImages
+dataset_folder = Data.path
+christmas_dataset = ChristmasImages(path=dataset_folder, training=True)
+
+# Access the class names
+class_names = christmas_dataset.dataset.classes
+
+output =len(class_names)
 
 class Network(nn.Module):
     def __init__(self):
         super().__init__()        
         self.model = torchvision.models.efficientnet_b4(pretrained = True)
-        self.model.fc = nn.Sequential(nn.Linear(1792, 8))
+        self.model.fc = nn.Sequential(nn.Linear(1792, output))
 
     def forward(self, x):       
         x = self.model(x)
