a40c14f0d7b8fae7dfaaef02ff6c4d80e07145fa
Akudavale <kudavale3105@gmail.com>
2024-01-25 21:04:44 +0100

    error updates

--- data.py
+++ data.py
@@ -4,15 +4,16 @@ from torchvision.datasets import ImageFolder
 import os
 from PIL import Image
 import natsort
+from pathlib import Path
 
 class ChristmasImages(Dataset):
     def __init__(self, path, training=True):
         super().__init__()
         self.training = training
-        self.path = path
+        self.path = Path(path)
         
         #For training data
-        self.transform1 = transforms.Compose([
+        self.train_transform = transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.RandomHorizontalFlip(p=0.4),
@@ -23,31 +24,31 @@ class ChristmasImages(Dataset):
             ])
         
         #For validation data
-        self.transform2 = transforms.Compose([
+        self.test_transform = transforms.Compose([
             transforms.Resize(256),
             transforms.ToTensor(),
             transforms.Normalize(mean= ([0.485, 0.456, 0.406]),std = ([0.229, 0.224, 0.225]))
             ])
         
         if self.training == True:
-            self.dataset = ImageFolder(root=self.path.joinpath('train'),transform=self.transform1)
+            self.dataset = ImageFolder(root=self.path.joinpath('train'),transform=self.train_transform)
         else:
             self.path = path
-            self.sorted_images = natsort.natsorted(os.listdir(self.path))          
+            self.img_sort = natsort.natsorted(os.listdir(self.path))          
             
     def __len__(self):
         if self.training:
             return len(self.dataset)
         else: 
-            return len(self.sorted_images)
+            return len(self.img_sort)
 
     def __getitem__(self, index):       
         if self.training == True:
             image, label = self.dataset[index]
             return image, label
         else:           
-            img = os.path.join(self.path,self.sorted_images[index])            
-            image = self.transform2(Image.open(img).convert("RGB")) 
+            img = os.path.join(self.path,self.img_sort[index])            
+            image = self.test_transform(Image.open(img).convert("RGB")) 
             return image
 
 
